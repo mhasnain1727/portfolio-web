b857712f3c41f4a75a4bf8ad68cc8b174f55f88f
Mohd <mohd.hasnain@peopletech.com>
2025-01-22 23:59:04 +0530

    did modification wrt landing page and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
